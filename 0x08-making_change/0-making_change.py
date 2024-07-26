e9d847c245abf012fe78b4a2c2d6a3049a193cf9
HudaOmer <hudaoe175@gmail.com>
2024-07-25 17:29:22 -0700

    Solve Task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,16 +1,13 @@
+#!/usr/bin/python3
+
 def makeChange(coins, total):
-    # Handle edge cases
     if total <= 0:
         return 0
-    
-    # Initialize the DP array
+
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
-    
-    # Fill the DP table
     for coin in coins:
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
-    
-    # Check the result
+
     return dp[total] if dp[total] != float('inf') else -1
