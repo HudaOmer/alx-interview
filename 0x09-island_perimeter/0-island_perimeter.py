d596a1cf9a3eadd9f0588d5c9870bcd0575cd8b0
HudaOmer <hudaoe175@gmail.com>
2024-07-30 16:12:04 -0700

    Solve Task 0

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+0. Island Perimeter
+"""
+
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid
+    """
+    n = len(grid[0])
+    perimeter = 0
+    for i in range (1, n - 1):
+        for j in range (1, n - 1):
+            if grid[i][j] == 0:
+                pass
+            else:
+                if grid[i - 1][j] == 0:
+                    perimeter += 1
+                if grid[i + 1][j] == 0:
+                    perimeter += 1
+                if grid[i][j - 1] == 0:
+                    perimeter += 1
+                if grid[i][j + 1] == 0:
+                    perimeter += 1
+    return perimeter
