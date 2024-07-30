1b78e301c7f5afb02c8d6afbaa6655a0157bc9dd
HudaOmer <hudaoe175@gmail.com>
2024-07-30 16:56:04 -0700

    Solve Task 0

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -13,15 +13,15 @@ def island_perimeter(grid):
     perimeter = 0
     for i in range(n):
         for j in range(m):
-            if grid[i][j] == 0:
-                pass
-            else:
-                if (i > 0 and grid[i - 1][j] == 0):
-                    perimeter += 1
-                if (i < n - 1 and grid[i + 1][j] == 0):
-                    perimeter += 1
-                if (j > 0 and grid[i][j - 1] == 0):
-                    perimeter += 1
-                if (j < m - 1 and grid[i][j + 1] == 0):
-                    perimeter += 1
+            if grid[i][j] == 1:
+                cell = 4
+                if (i > 0 and grid[i - 1][j] == 1):
+                    cell -= 1
+                if (i < n - 1 and grid[i + 1][j] == 1):
+                    cell -= 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    cell -= 1
+                if (j < m - 1 and grid[i][j + 1] == 1):
+                    cell -= 1
+                perimeter += cell
     return perimeter
